8ed71e49c60e1028e391786ef72cbd934a47e2ea
Sharp0802 <sharp0802.business@gmail.com>
2025-08-29 22:51:50 +0900

    :white_check_mark: Add test

---
+++ test/main.cxx
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "ubench.h"
+
+unsigned int seed;
+
+static void prepare(ubench::args &state) {
+  state = { 5, 10, 15 };
+}
+
+bool is_prime(const int v) {
+  for (int i = 2; i * i <= v; i++) {
+    if (v % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int nth_prime(const int nth) {
+  for (auto i = 2, n = 0; ; ++i) {
+    if (is_prime(i)) {
+      if (++n == nth)
+        return i;
+    }
+  }
+}
+
+static void test(const ubench::arg nth) {
+  nth_prime(nth);
+}
+
+BENCHMARK(test).prepare(prepare);
+
+int main() {
+  const auto entries = ubench::run();
+  ubench::print(entries);
+
+  return 0;
+}
